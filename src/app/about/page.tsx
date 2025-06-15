8602d709dab05d546e742f6cb3a2bfa4989427ff
jomapisa <pino.josemanuel@gmail.com>
2025-06-15 23:56:03 +0800

    fill navbar, create footer, additional styling

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,14 +1,20 @@
+"use client"
 
+import {useTheme} from "@/context/ThemeContext";
 
 export default function About() {
 
+    const theme = useTheme();
+    
+    const bgColor = theme.theme === "light" ? "bg-white" : "bg-gray-800";
+    const textColor = theme.theme === "light" ? "" : "text-white";
 
     return(
-        <div>
-            <h1 className="font-extrabold text-4xl">About</h1>
+        <div  className={`h-[85vh] w-full px-36 py-18 ${bgColor} ${textColor}`}>
+            <h1 className="font-extrabold text-4xl my-8">About</h1>
             <p>This is Jose's React + Next.js learning project. </p>
-            <h2 className="font-bold text-3xl">Placeholders</h2>
-            <h3 className="font-bold text-2xl">We all love placeholders</h3>
+            <h2 className="font-bold text-3xl my-6">Placeholders</h2>
+            <h3 className="font-bold text-2xl my-4">We all love placeholders</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
     )
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,9 +1,17 @@
+"use client"
 
+import { useTheme } from "@/context/ThemeContext";
 
 export default function Contact() {
+
+    const theme = useTheme();
+
+    const bgColor = theme.theme === "light" ? "bg-white" : "bg-gray-800";
+    const textColor = theme.theme === "light" ? "" : "text-white"; 
+
     return(
-        <div>
-            <h1 className="font-extrabold text-4xl">Contact</h1>
+        <div className={`h-[85vh] w-full p-36 ${bgColor} ${textColor}`}>
+            <h1 className="font-extrabold text-4xl my-8">Contact</h1>
             <p>You can contact us by phone or email</p>
             <p>+852 1234 5678</p>
             <p>enquiries@some.domain.com</p>
